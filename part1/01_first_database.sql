37878ed68db640ff27515b96700c1dfb18063540
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-01 15:59:51 +0530

    Not Null query added Done

--- part1/01_first_database.sql
+++ part1/01_first_database.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE shopDB
+CREATE DATABASE school
 
-DROP DATABASE shopDB 
+DROP DATABASE school
 
-DROP DATABASE shopDB with (FORCE)
+DROP DATABASE school with (FORCE)
 
 // first query to genarate a Table
 
@@ -15,4 +15,15 @@ enroll_data DATE DEFAULT CURRENT_DATE
 );
 
 // query for NOT NULL
+CREATE TABLE student (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(100) NOT NULL,
+   class INT CHECK (class BETWEEN 1 AND 12),
+   marks INT CHECK (marks BETWEEN 0 AND 100),
+   city VARCHAR(50),
+   gender CHAR(1) CHECK (gender IN ('M','F')),
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+select * from users
 
+select NULL =0;
